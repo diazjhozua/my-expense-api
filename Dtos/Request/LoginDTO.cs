2fd75b75ee5dddc1a42ea4c3c26c783010602855
Jhozua Diaz <diaz.jhozua@gmail.com>
2022-07-27 23:51:00 +0800

    Created DTO for user login and register

--- Dtos/Request/LoginDTO.cs
+++ Dtos/Request/LoginDTO.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,11 @@ namespace my_expense_api.Dtos.Request
 {
     public class LoginDTO
     {
-        
+        [Required]
+        [StringLength(100)]
+        [Components.Validators.EmailAddressAttribute]
+        public string Email { get; set; }
+        [StringLength(30)]
+        public string Password { get; set; }       
     }
 }
