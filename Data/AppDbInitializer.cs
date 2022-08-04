f50c541cd8408c95d7b75a759ff836149ca3fe19
Jhozua Diaz <diaz.jhozua@gmail.com>
2022-08-04 19:36:16 +0800

    Completed DBInitializer

--- Data/AppDbInitializer.cs
+++ Data/AppDbInitializer.cs
@@ -23,10 +23,34 @@ namespace my_expense_api.Data
                 if (!env.IsProduction())
                 {
                     if (!context.Users.Any()) {
-                        for (int i = 0; i < 5; i++) 
+                        var hmac = new System.Security.Cryptography.HMACSHA512();
+                        context.Users.Add(new User() {Email = "sample@gmail.com",FirstName = Faker.Name.First(), LastName = Faker.Name.Last(), PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes("sample123")), PasswordSalt = hmac.Key });
+                    }
+
+                    context.SaveChanges();
+
+                    if (!context.Categories.Any()) {
+                        
+                        context.Categories.Add(new Category{Name= "School", Limit=3200, UserId=1});
+                        context.Categories.Add(new Category{Name= "General", Limit=3200, UserId=1});
+                        context.Categories.Add(new Category{Name= "Work", Limit=3200, UserId=1});
+                        context.Categories.Add(new Category{Name= "Hobby", Limit=3200, UserId=1});
+                        context.Categories.Add(new Category{Name= "Emergency", Limit=3200, UserId=1});
+                    }
+
+                    context.SaveChanges(); 
+
+                    if (!context.Expenses.Any()) {
+                        for (int i = 0; i < 100; i++) 
                         {
-                            var hmac = new System.Security.Cryptography.HMACSHA512();
-                            context.Users.Add(new User() {Email = Faker.Internet.Email(),FirstName = Faker.Name.First(), LastName = Faker.Name.Last(), PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes("sample123")), PasswordSalt = hmac.Key });
+                            var types = new[] { ExpenseType.Cash, ExpenseType.Cheque, ExpenseType.CreditCard, ExpenseType.DebitCard };
+                            Random rnd = new Random();
+                            DateTime date = new DateTime(2022, 1, 1);
+                            int range = (DateTime.Today - date).Days;
+                            context.Expenses.Add(new Expense{Name= Faker.Lorem.Sentence(2), 
+                                Cost= Faker.RandomNumber.Next(1, 10000), 
+                                Description = Faker.Lorem.Paragraph(3), Type = types[rnd.Next(types.Length)], 
+                                UserId = 1, CategoryId = Faker.RandomNumber.Next(1,5), Date = date.AddDays(rnd.Next(range))});
                         }
                     }
 
--- Models/Expense.cs
+++ Models/Expense.cs
@@ -10,9 +10,11 @@ namespace my_expense_api.Models
         public string Name { get; set; }     
         public float Cost { get; set; } 
         public string Description { get; set; } 
-        public ExpenseType Type {get; set;}
+        public ExpenseType Type {get; set;} 
         public User User { get; set; }
-        public Category Category { get; set; }   
+        public int UserId { get; set; }
+        public Category Category { get; set; } 
+        public int CategoryId { get; set; }  
         public DateTime Date { get; set; }     
     }
 }
